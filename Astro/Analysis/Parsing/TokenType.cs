2ebc9ccf9059f530c3fd60da75145b8c150fe510
Axel Snaer <axelatlason2003@gmail.com>
2022-08-31 14:40:31 +0000

    Added common math operators and EndOfFile to the scanner

---
+++ Astro/Analysis/Parsing/TokenType.cs
@@ -0,0 +1,39 @@
+﻿namespace AstroLang.Analysis.Parsing;
+
+public enum TokenType
+{
+	// Variable character tokens
+	Identifier,
+	Number,
+	String,
+
+	// Set character tokens
+	LeftParen,
+	RightParen,
+	LeftBracket,
+	RightBracket,
+	LeftBrace,
+	RightBrace,
+	Equals,
+	DoubleEquals,
+	BangEquals,
+	Lesser,
+	LesserEquals,
+	Greater,
+	GreaterEquals,
+	Dot,
+	Semicolon,
+	Bang,
+	Plus,
+	Minus,
+	Star,
+	Slash,
+	Percent,
+	
+	// Keywords
+	True,
+	False,
+	
+	// Special
+	EndOfFile,
+}
