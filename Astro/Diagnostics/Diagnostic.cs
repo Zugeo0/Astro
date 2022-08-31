652e4de458896296af81c93f8d8251941773a8b0
Axel Snaer <axelatlason2003@gmail.com>
2022-08-31 13:21:30 +0000

    Made the error message use the end of the span instead of the start

--- Astro/Diagnostics/Diagnostic.cs
+++ Astro/Diagnostics/Diagnostic.cs
@@ -17,7 +17,7 @@ public class Diagnostic
 	{
 		var lineIdx = source.FindLineIndex(Span.Start);
 		var line = source.GetLine(lineIdx);
-		var column = Span.Start - line.Span.Start;
+		var column = Span.Start - line.Span.Start + Span.Length - 1;
 		writer.WriteLine($"Error: {Message} at [{lineIdx + 1}:{column}]");
 
 		var lineNumberText = $"{lineIdx + 1} | ";
